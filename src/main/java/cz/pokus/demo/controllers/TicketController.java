dc74dae4f4cb1884602032c98ef2aa3ed9b82e5f
j.zahradnik <j.zahradnik@neopost.com>
2019-12-20 14:16:26 +0100

    Cool buttons

--- src/main/java/cz/pokus/demo/controllers/TicketController.java
+++ src/main/java/cz/pokus/demo/controllers/TicketController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-//import java.util.concurrent.atomic.AtomicLong;
+
 
 @Controller
 public class TicketController {
@@ -19,46 +19,6 @@ public class TicketController {
         this.ticketService = ticketService;
     }
 
-
-
-//    @PostMapping("/tickets")
-//    public Ticket createNewTicket(@RequestBody Ticket ticket){
-//        ticket.setId(nextId.incrementAndGet());
-//
-//        tickets.add(ticket);
-//
-//        return ticket;
-//    }
-
-//    @GetMapping("/tickets")
-//    public List<Ticket> getAllTickets(){
-//
-//        return tickets;
-//    }
-
-
-//    @GetMapping("/tickets/{id}")
-//    public Ticket getOneTicket(@PathVariable("id") long ticketId){
-//
-//        for(Ticket ticket : tickets){
-//            if(ticket.getId() == ticketId ){
-//                return ticket;
-//            }
-//        }
-//        throw new IllegalArgumentException();
-//    }
-
-//    @PostMapping("/tickets")
-//    public Ticket editOneTicket(@PathVariable("id") int ticketId, @RequestBody Ticket newTicket){
-//        for(Ticket ticket : tickets){
-//            if(ticket.getId() == ticketId ){
-//                tickets.remove(ticket);
-//                newTicket.setId(ticketId);
-//                return ticket;
-//            }
-//        }
-//        throw new IllegalArgumentException();
-//    }
 
     @GetMapping(value = "/")
     public String index(Model model) {
