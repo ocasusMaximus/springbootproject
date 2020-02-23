4eee207ea821930c8ed08e73fe2996be506a068f
ocasusMaximus <za.jan@seznam.cz>
2020-02-23 17:08:36 +0100

    Create, update, delete and delete all with connection to hall capacity solved. Even capacity of hall is changing directly to actual occupancy. Requirement for it to be > 0 done.

--- src/main/java/cz/pokus/demo/controllers/RestController.java
+++ src/main/java/cz/pokus/demo/controllers/RestController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -16,41 +17,50 @@ import java.util.Map;
 
 
 @Controller
-public class TicketController {
+public class RestController {
 
 
     private final TicketService ticketService;
     private HallService hallService;
 
     @Autowired
-    public TicketController(TicketService ticketService, HallService hallService) {
+    public RestController(TicketService ticketService, HallService hallService) {
         this.ticketService = ticketService;
         this.hallService = hallService;
     }
 
+    @PostConstruct
+    public void init(){
+        hallService.createHall(new Hall(1,"J2",20));
+        hallService.createHall(new Hall(2,"J3",20));
+        for(int i =0; i < hallService.loadAllHalls().size(); i++){
+            System.out.println("Na pozici: " + i +" Id: " + hallService.loadAllHalls().get(i).getId() + " hala: "+ hallService.loadAllHalls().get(i).getName() + " kapacita:" + hallService.loadAllHalls().get(i).getCapacity());
+        }
 
+    }
 
     @GetMapping(value = "/")
     public String index(Model model) {
         model.addAttribute("tickets", ticketService.loadAllTickets());
-        model.addAttribute("halls", hallService.loadAllHalls());
         return "tickets";
     }
     @PostMapping(value = "/createTicket")
     public String createTicket(@ModelAttribute Ticket ticket, Model model) {
-
         int idOfHall = hallService.getIdOfHall(ticket);
-        int capacity =hallService.loadHallById(idOfHall).getCapacity() -ticketService.getNumberOfTakenSeats(ticket);
+        Hall updatedHall = hallService.loadHallById(idOfHall);
+        System.out.println("Jsem v creatu pred pridanim do modelu: "+hallService.loadHallById(idOfHall).getCapacity());
+        int capacity =hallService.loadHallById(idOfHall).getCapacity() ;//-ticketService.getNumberOfTakenSeats(ticket);
         if (capacity>= ticket.getNumberOfSeats()){
             ticketService.createTicket(ticket);
-           hallService.loadHallById(idOfHall).setCapacity(capacity- ticket.getNumberOfSeats());
+            hallService.loadHallById(idOfHall).setCapacity(capacity- ticket.getNumberOfSeats());
+            hallService.createHall(updatedHall);
             System.out.println("Při creatu:" + hallService.loadHallById(idOfHall).getCapacity() + " nazev " + hallService.loadHallById(idOfHall).getName());
 
     } else {
            return "error";
         }
         model.addAttribute("tickets", ticketService.loadAllTickets());
-
+        System.out.println("Jsem v creatu po pridani do modelu: "+hallService.loadHallById(idOfHall).getCapacity());
         return "redirect:/";
     }
 
@@ -58,14 +68,30 @@ public class TicketController {
 
     @PostMapping(value = "/deleteTicket")
     public String deleteTicket(@ModelAttribute Ticket ticket, Model model) {
+        int tickNoS=ticket.getNumberOfSeats();
+        int idOfHall = hallService.getIdOfHall(ticket);
+        Hall updatedHall = hallService.loadHallById(idOfHall);
+        int capacity = updatedHall.getCapacity() + tickNoS;
         ticketService.removeTicket(ticket);
+        updatedHall.setCapacity(capacity);
+        hallService.createHall(updatedHall);
+
         model.addAttribute("tickets", ticketService.loadAllTickets());
         return "redirect:/";
     }
     @PostMapping(value = "/deleteAllTickets")
-    public String deleteAllTickets(@ModelAttribute Ticket ticket, Model model) {
+    public String deleteAllTickets(Model model) {
+        for(Ticket ticket: ticketService.loadAllTickets()) {
+            int tickNoS = ticket.getNumberOfSeats();
+            int idOfHall = hallService.getIdOfHall(ticket);
+            Hall updatedHall = hallService.loadHallById(idOfHall);
+            int capacity = updatedHall.getCapacity() + tickNoS;
+            updatedHall.setCapacity(capacity);
+            hallService.createHall(updatedHall);
+        }
         ticketService.deleteAllTickets();
         model.addAttribute("tickets", ticketService.loadAllTickets());
+        model.addAttribute("halls", hallService.loadAllHalls());
         return "redirect:/";
     }
     @GetMapping(value = "/addTicket")
@@ -79,23 +105,29 @@ public class TicketController {
     @GetMapping(value = "/updateTicket")
     public String getUpdateTickets(Model model, @ModelAttribute Ticket ticket){
         model.addAttribute("tickets",ticketService.loadAllTickets());
+        model.addAttribute("halls", hallService.loadAllHalls());
         return "addTicket";
 
     }
 
    @PostMapping (value = "/ticketUpdate/{id}")
     public String updateTicket( @ModelAttribute Ticket ticket, @PathVariable int id, Model model) {
+        int oldTicketNoS =ticketService.loadTicketById(id).getNumberOfSeats();
+        System.out.println("oldticketNoS: " + oldTicketNoS);
         ticket.setId(id);
        int idOfHall = hallService.getIdOfHall(ticket);
+       Hall updatedHall = hallService.loadHallById(idOfHall);
        System.out.println("prvni" +hallService.loadHallById(idOfHall).getCapacity());
-       //problem je v tom ze capacita je tady zadana a uz je aktualne snizena takze snizuji od snizene
-       int capacity =hallService.loadHallById(idOfHall).getCapacity() -ticketService.getNumberOfTakenSeats(ticket);
+
+       int capacity =hallService.loadHallById(idOfHall).getCapacity() + oldTicketNoS;
        System.out.println(capacity);
        if (capacity>= ticket.getNumberOfSeats()){
+           hallService.loadHallById(idOfHall).setCapacity(capacity);
            System.out.println("druha " +hallService.loadHallById(idOfHall).getCapacity());
            ticketService.updateTicket(ticket);
-           capacity =hallService.loadHallById(idOfHall).getCapacity() -ticketService.getNumberOfTakenSeats(ticket);
-           hallService.loadHallById(idOfHall).setCapacity(capacity);
+           capacity =hallService.loadHallById(idOfHall).getCapacity();
+           hallService.loadHallById(idOfHall).setCapacity(capacity - ticket.getNumberOfSeats());
+           hallService.createHall(updatedHall);
            System.out.println("capacity"+capacity);
            System.out.println("treti: " +hallService.loadHallById(idOfHall).getCapacity());
         } else  {
